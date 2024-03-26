1fb4a7adaaac7c406d150fb8b71ac07e9c41172b
unknown <mariafizameharoof@gmail.com>
2024-03-26 09:40:35 +0530

    api and alert: error is showing while signing up

--- src/Screens/Login/index.js
+++ src/Screens/Login/index.js
@@ -1,41 +1,67 @@
-import React, { useState } from "react";
-import { View, Text, StyleSheet, TouchableOpacity, ScrollView } from 'react-native'
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, ScrollView, Alert } from 'react-native';
 import { TextInput, useTheme } from 'react-native-paper';
 
+const Index = (props) => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [emailError, setEmailError] = useState('');
+    const [passwordError, setPasswordError] = useState('');
+    const theme = useTheme();
 
-const index = (props) => {
-    const theme = useTheme()
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+    const { email: storedEmail, password: storedPassword } = props.route.params || { email: '', password: '' };
 
-    async function postJSON(email,password) {
-        try {
-          const postData={
-           email: email,
-            password: password
-      
-          }
-          const headers = {
-            'device-id': 'd12121',
-            'app-type': 'web'
-          };
-          const response = await fetch("https://api.dev.returnredirect.com/api/1.0/auth/login", {
-            method: "POST", // or 'PUT'
-            headers: headers,
-            body: JSON.stringify(postData),
-          });
-      
-          const result = await response.json();
-          console.log("Success:", result);
-        } catch (error) {
-          console.error("Error:", error);
+
+    const handleSubmit = async () => {
+        if (!email.trim()) {
+          setEmailError('Email is required');
+        } else {
+          setEmailError('');
         }
-      }
-      
-      //const data = { username: "example" };
-      //postJSON(); 
-    return (
+    
+        if (!password.trim()) {
+          setPasswordError('Password is required');
+        } else {
+          setPasswordError('');
+        }
+    
+           
+        if (email.trim() && password.trim() ) {
+          try {
+            const response = await fetch('https://api.dev.returnredirect.com/api/1.0/auth/login', {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+              body: JSON.stringify({
+            
+                email,
+                password,
+                }),
+            });
+    
+            if (response.ok) {
+              // Sign up successful
+              console.log('Sign up successful');
+              // Navigate to the login page
+              props.navigation.navigate('Login');
+            } else {
+              // Sign up failed
+              const errorData = await response.json();
+              console.log('Sign up error:', errorData.error);
+              //Alert.alert('Error', errorData.error);
+            }
+          } catch (error) {
+            console.log('Network error:', error);
+            //Alert.alert('Error', 'Network error');
+          }
+        }
+      };
 
+
+   
+
+    return (
         <ScrollView style={[styles.container, { backgroundColor: theme.colors.primary }]}>
             <View style={{ justifyContent: "center", alignItems: "center", padding: 100 }}>
                 <Text style={{ color: "#fff", fontSize: 40 }}>EXPO</Text>
@@ -50,26 +76,30 @@ const index = (props) => {
                         style={{ backgroundColor: "#fff" }}
                         underlineColor='transparent'
                         activeUnderlineColor={theme.colors.primary}
+                        value={email}
                         onChangeText={setEmail}
+                        error={emailError ? true : false}
                     />
+                    {emailError ? <Text style={styles.error}>{emailError}</Text> : null}
                     <TextInput
                         label="Password"
                         style={{ backgroundColor: "#fff", marginTop: 20 }}
                         underlineColor='transparent'
                         activeUnderlineColor={theme.colors.primary}
                         secureTextEntry={true}
+                        value={password}
                         onChangeText={setPassword}
+                        error={passwordError ? true : false}
                     />
-
-                    <TouchableOpacity  style={[styles.loginButton, { backgroundColor: theme.colors.primary }]} onPress={() =>  postJSON(email, password)}>
-                                <Text style={{ color: "#fff", fontSize: 20 }}>Login</Text>
-                                
+                    {passwordError ? <Text style={styles.error}>{passwordError}</Text> : null}
+                    <TouchableOpacity onPress={handleSubmit} style={[styles.loginButton, { backgroundColor: theme.colors.primary }]}>
+                        <Text style={{ color: "#fff", fontSize: 20 }}>Login</Text>
                     </TouchableOpacity>
                 </View>
                 <View style={{ height: 80, alignItems: 'center', flexDirection: 'row', justifyContent: 'center', }} />
             </View>
             <View style={{ backgroundColor: "#f0f0f0", height: 120, justifyContent: "center", alignItems: "center", flexDirection: 'row' }}>
-                <Text style={{}}>Dont't have any account ? </Text>
+                <Text style={{}}>Don't have any account? </Text>
                 <TouchableOpacity onPress={() => {
                     props.navigation.navigate('Signup')
                 }}>
@@ -78,10 +108,7 @@ const index = (props) => {
             </View>
         </ScrollView>
     );
-
 }
-
-export default index
 
 const styles = StyleSheet.create({
     container: {
@@ -94,5 +121,12 @@ const styles = StyleSheet.create({
         borderRadius: 5,
         marginTop: 20,
         minHeight: 65
+    },
+    error: {
+        color: 'red',
+        fontSize: 14,
+        marginTop: 5,
     }
-})
+});
+
+export default Index;
--- src/Screens/Signup/index.js
+++ src/Screens/Signup/index.js
@@ -1,48 +1,91 @@
-import React, { useState } from "react";
-import { View, Text, StyleSheet, TouchableOpacity, ScrollView } from 'react-native'
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, ScrollView, Alert } from 'react-native';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import { TextInput, useTheme } from 'react-native-paper';
 
+const Index = (props) => {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [phone, setPhone] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
 
-const index = (props) => {
-    const theme = useTheme()
-    const [name, setName] = useState("");
-    const [email, setEmail] = useState("");
-    const [phoneNo, setPhoneNo] = useState("")
-    const [password, setPassword] = useState("");
-    const [confirmPassword, setConfirmPassword] = useState("");
-    async function postJSON(name,email, phoneNo,password, confirmPassword) {
-        try {
-          const postData={
-            name: name,
-            email: email,
-            phoneNumber: phoneNo,
-            password: password,
-            confirmPassword: confirmPassword
-      
-          }
-          const headers = {
-            'device-id': 'd12121',
-            'app-type': 'web'
-          };
-          const response = await fetch("https://api.dev.returnredirect.com/api/1.0/auth/signup", {
-            method: "POST", // or 'PUT'
-            headers: headers,
-            body: JSON.stringify(postData),
-          });
-      
-          const result = await response.json();
-          console.log("Success:", result);
-        } catch (error) {
-          console.error("Error:", error);
+    const [nameError, setNameError] = useState('');
+    const [emailError, setEmailError] = useState('');
+    const [phoneError, setPhoneError] = useState('');
+    const [passwordError, setPasswordError] = useState('');
+    const [confirmPasswordError, setConfirmPasswordError] = useState('');
+    const theme = useTheme();
+
+    const handleSubmit = async () => {
+        if (!email.trim()) {
+          setEmailError('Email is required');
+        } else {
+          setEmailError('');
         }
-      }
-      
-     // const data = { username: "example" };
-      //postJSON(); 
+    
+        if (!password.trim()) {
+          setPasswordError('Password is required');
+        } else {
+          setPasswordError('');
+        }
+    
+        if (!name.trim()) {
+          setNameError('Name is required');
+        } else {
+          setNameError('');
+        }
+    
+        if (!phone.trim()) {
+          setPhoneError('Phone number is required');
+        } else {
+          setPhoneError('');
+        }
+    
+        if (!confirmPassword.trim()) {
+          setConfirmPasswordError('Confirm Password is required');
+        } else if (password !== confirmPassword) {
+          setConfirmPasswordError('Passwords do not match');
+        } else {
+          setConfirmPasswordError('');
+        }
+    
+        if (email.trim() && password.trim() && name.trim() && phone.trim() && confirmPassword.trim() && password === confirmPassword) {
+          try {
+            const response = await fetch('https://api.dev.returnredirect.com/api/1.0/auth/signup', {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+              body: JSON.stringify({
+                name,
+                email,
+                phoneNumber: phone,
+                password,
+                confirmPassword,
+              }),
+            });
+    
+            if (response.ok) {
+              // Sign up successful
+              console.log('Sign up successful');
+              // Navigate to the login page
+              props.navigation.navigate('Login');
+            } else {
+              // Sign up failed
+              const errorData = await response.json();
+              console.log('Sign up error:', errorData.error);
+              Alert.alert('Error', errorData.error);
+            }
+          } catch (error) {
+            console.log('Network error:', error);
+            Alert.alert('Error', 'Network error');
+          }
+        }
+      };
 
+   
     return (
-
         <ScrollView style={[styles.container, { backgroundColor: theme.colors.primary }]}>
             <View style={{ justifyContent: 'space-between', alignItems: "center", padding: 50, flexDirection: "row" }}>
                 <MaterialCommunityIcons name='arrow-left' size={40} color={"#fff"} onPress={() => { props.navigation.goBack() }} />
@@ -56,43 +99,56 @@ const index = (props) => {
                         style={{ backgroundColor: "#fff" }}
                         underlineColor='transparent'
                         activeUnderlineColor={theme.colors.primary}
+                        value={name}
                         onChangeText={setName}
+                        error={nameError ? true : false}
                     />
+                    {nameError ? <Text style={styles.error}>{nameError}</Text> : null}
                     <TextInput
                         label="Email"
                         style={{ backgroundColor: "#fff", marginTop: 20 }}
                         underlineColor='transparent'
                         activeUnderlineColor={theme.colors.primary}
+                        value={email}
                         onChangeText={setEmail}
+                        error={emailError ? true : false}
                     />
-                          <TextInput
+                    {emailError ? <Text style={styles.error}>{emailError}</Text> : null}
+                    <TextInput
                         label="Phone"
                         style={{ backgroundColor: "#fff", marginTop: 20 }}
                         underlineColor='transparent'
                         activeUnderlineColor={theme.colors.primary}
-                        onChangeText={setPhoneNo}
-                        
+                        value={phone}
+                        onChangeText={setPhone}
+                        error={phoneError ? true : false}
                     />
+                    {phoneError ? <Text style={styles.error}>{phoneError}</Text> : null}
                     <TextInput
                         label="Password"
                         style={{ backgroundColor: "#fff", marginTop: 20 }}
                         underlineColor='transparent'
                         activeUnderlineColor={theme.colors.primary}
                         secureTextEntry={true}
+                        value={password}
                         onChangeText={setPassword}
+                        error={passwordError ? true : false}
                     />
+                    {passwordError ? <Text style={styles.error}>{passwordError}</Text> : null}
                     <TextInput
                         label="Confirm Password"
                         style={{ backgroundColor: "#fff", marginTop: 20 }}
                         underlineColor='transparent'
                         activeUnderlineColor={theme.colors.primary}
                         secureTextEntry={true}
+                        value={confirmPassword}
                         onChangeText={setConfirmPassword}
+                        error={confirmPasswordError ? true : false}
                     />
+                    {confirmPasswordError ? <Text style={styles.error}>{confirmPasswordError}</Text> : null}
 
-                    <TouchableOpacity style={[styles.signupButton, { backgroundColor: theme.colors.primary }]} onPress={() => postJSON(name, email, phoneNo, password, confirmPassword)}>
-                            <Text style={{ color: "#fff", fontSize: 20 }}>Sign Up</Text>
-                            
+                    <TouchableOpacity onPress={handleSubmit} style={[styles.signupButton, { backgroundColor: theme.colors.primary }]}>
+                        <Text style={{ color: "#fff", fontSize: 20 }}>Sign Up</Text>
                     </TouchableOpacity>
                 </View>
 
@@ -107,10 +163,7 @@ const index = (props) => {
             </View>
         </ScrollView>
     );
-
 }
-
-export default index
 
 const styles = StyleSheet.create({
     container: {
@@ -123,5 +176,12 @@ const styles = StyleSheet.create({
         borderRadius: 5,
         marginTop: 20,
         minHeight: 65
+    },
+    error: {
+        color: 'red',
+        fontSize: 14,
+        marginTop: 5,
     }
-})
+});
+
+export default Index;

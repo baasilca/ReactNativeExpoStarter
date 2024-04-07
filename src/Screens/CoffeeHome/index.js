7f2892800f1a0336babb1c98ccf5d1e857d49831
PoojitaShanmugam <1ms20ec062@msrit.edu>
2024-04-07 21:38:16 +0530

    search feature-filter() and includes()

--- src/Screens/CoffeeHome/index.js
+++ src/Screens/CoffeeHome/index.js
@@ -1,11 +1,11 @@
-import React, { useState, useEffect } from "react"
-import { StyleSheet, Text, View, TextInput, FlatList, Image, ScrollView, SafeAreaView, TouchableOpacity } from "react-native"
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View, TextInput, FlatList, Image, ScrollView, TouchableOpacity } from "react-native";
 
 export default function Index(props) {
-  const [data, setData] = useState([])
+  const [data, setData] = useState([]);
   const [selectedCoffee, setSelectedCoffee] = useState('hot');
   const [searchText, setSearchText] = useState('');
-  const [loading, setloading] = useState(true)
+  const [loading, setLoading] = useState(true);
 
   const url = `https://api.sampleapis.com/coffee/${selectedCoffee}`;
 
@@ -22,28 +22,19 @@ export default function Index(props) {
     setSelectedCoffee(name);
   };
 
-
-
-
-  const filteredCoffeeNames = CoffeeNames.filter(coffee =>
-    coffee.name.toLowerCase().includes(searchText.toLowerCase())
-  );
-
-
-
   useEffect(() => {
     fetch(url)
       .then((response) => response.json())
       .then((json) => setData(json))
       .catch((error) => console.error(error))
-      .finally(() => setloading(false))
-  }, [selectedCoffee])
+      .finally(() => setLoading(false));
+  }, [selectedCoffee]);
+
+  const filteredData = data.filter(coffee => coffee.title.toLowerCase().includes(searchText.toLowerCase()));
 
   const renderItem = ({ item, index }) => {
-
     return (
-
-      <View style={{ marginLeft: index % 2 != 0 ? 10 : 0, backgroundColor: "#fff", padding: 5, marginBottom: 10 }}>
+      <View style={{ marginLeft: index % 2 !== 0 ? 10 : 0, backgroundColor: "#fff", padding: 5, marginBottom: 10 }}>
         <Image
           style={styles.image}
           source={{ uri: item.image }}
@@ -52,44 +43,19 @@ export default function Index(props) {
         <Text numberOfLines={2} ellipsizeMode='tail' style={{ fontSize: 15, fontWeight: "bold" }}>Description: {item.description.slice(0, 10)}</Text>
         <View style={{ flexDirection: 'row' }}>
           <Text style={{ fontSize: 18 }}>   Price: $4.7   </Text>
-          {/* <View style={styles.spacefl} /> */}
           <TouchableOpacity style={{ backgroundColor: '#f4a460', padding: 5, borderRadius: 10, alignSelf: "flex-end" }}>
             <Text style={{ color: '#fff', fontSize: 20 }}>  +  </Text>
           </TouchableOpacity>
         </View>
-        {/*        
-    <Image
-        style={styles.image}
-        source={{ uri: item.image }}
-      />
-      <View>
-      <Text style={{ fontSize: 25, fontWeight: "bold", alignSelf:"center" }}> {item.title}</Text>
-      <Text numberOfLines={2} ellipsizeMode='tail' style={{ fontSize: 15, fontWeight: "bold" }}>Description: {item.description}</Text>
-      <View style={styles.spacefl} />
-      <View style={{ flexDirection: 'row', justifyContent: 'flex-start', alignSelf:'flex-start' }}>
-        <Text style={{fontSize:25}}>   Price: $4.7   </Text>
-        <View style={styles.spacefl} />
-        <TouchableOpacity style={{ backgroundColor: '#f4a460', padding: 5, borderRadius: 10, alignSelf:"flex-end" }}>
-        <Text style={{ color: '#fff', fontSize:20}}>  +  </Text>
-        </TouchableOpacity>
       </View>
-    </View>
-    <Text style={styles.text}></Text>
-   
-     */}
-      </View>
-
-
     );
   };
 
   return (
-
     <View style={styles.container}>
-
       <View style={{ flexDirection: 'column', justifyContent: 'flex-start', alignSelf: 'flex-start' }}>
         <Text style={{ color: 'white' }}>Location:</Text>
-        <Text style={{ color: 'white', fontWeight: "bold", fontSize: 20 }}>Mathikere, Banglore</Text>
+        <Text style={{ color: 'white', fontWeight: "bold", fontSize: 20 }}>Indiranagar, Banglore</Text>
         <View style={styles.space} />
       </View>
 
@@ -100,46 +66,31 @@ export default function Index(props) {
         onChangeText={setSearchText}
         value={searchText}
         underlineColorAndroid="transparent"
-        borderWidth={2} // Increase the border thickness
+        borderWidth={2}
         width="90%"
         borderRadius={10}
-
       />
 
-      <Image
-        style={{ alignSelf: 'center' }}
-        source={require('./Frame1.png')}
-      />
       <View style={styles.space} />
-      <View style={{ flexDirection: 'row', justifyContent: 'flex-start', alignSelf: 'flex-start' }}>
-        <View style={styles.space} />
-        <ScrollView horizontal>
-          {CoffeeNames.map((coffee) => (
-            <TouchableOpacity key={coffee.id} onPress={() => handleCoffeePress(coffee.name)} style={[styles.coffeeButton,{ backgroundColor:selectedCoffee == coffee.name ? '#f4a460' : "black",}]}>
-              <Text style={styles.coffeeText}>{coffee.name}</Text>
-            </TouchableOpacity>
-          ))}
-        </ScrollView>
-      </View>
-
-
-
+      <ScrollView horizontal>
+        {CoffeeNames.map((coffee) => (
+          <TouchableOpacity key={coffee.id} onPress={() => handleCoffeePress(coffee.name)} style={[styles.coffeeButton, { backgroundColor: selectedCoffee == coffee.name ? '#f4a460' : "#fff" }]}>
+            <Text style={[styles.coffeeText, { color: selectedCoffee == coffee.name ? '#fff' : "#000" }]}>{coffee.name}</Text>
+          </TouchableOpacity>
+        ))}
+      </ScrollView>
 
       {loading ? (
         <Text>Loading...</Text>
       ) : (
-
         <FlatList
-          data={data}
+          data={filteredData}
           keyExtractor={(item) => item.id.toString()}
           renderItem={renderItem}
           numColumns={2}
-
         />
       )}
     </View>
-
-
   );
 }
 
@@ -150,19 +101,6 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
     padding: 20,
-
-
-  },
-  box: {
-    flexDirection: 'column',
-    justifyContent: 'flex-start',
-    alignItems: 'center',
-    padding: 5,
-    backgroundColor: 'white',
-    margin: 3,
-    marginVertical: 3,
-    marginBottom: 10,
-
   },
   image: {
     width: 160,
@@ -170,45 +108,18 @@ const styles = StyleSheet.create({
     borderRadius: 30,
     resizeMode: "contain",
   },
-  text: {
-    fontSize: 16,
-    color: "black"
-  },
-  Framepic: {
-    width: 300,
-    height: 150,
-    alignItems: "center",
-  },
   space: {
     width: 20,
     height: 20,
   },
-  spacefl: {
-    width: 75,
-    height: 15,
-  },
-  selectedCoffeeContainer: {
-    marginTop: 20,
-  },
-  selectedCoffeeText: {
-    fontSize: 20,
-    color: 'black',
-  },
   coffeeText: {
-    color: '#fff',
     fontSize: 16,
     textAlign: 'center',
-  },
-  coffeeContainer: {
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    justifyContent: 'center',
   },
   coffeeButton: {
     padding: 15,
     margin: 10,
     borderRadius: 10,
-
   },
   input: {
     height: 40,
@@ -216,8 +127,6 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     paddingHorizontal: 10,
     marginBottom: 10,
+    color: '#fff',
   },
-
-
-
-})
+});
